2b54c7f81f1c9f84361fa75c3f59d3b9ea54208d
ARTidas <artidas@artidas.hu>
2024-02-26 17:51:09 +0100

    Changed the VM creation process from VBoxManage to Virt + Virsh.

--- create_vm.sh
+++ create_vm.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 VMNAME="$1"
-
-# Check if VM name is provided
-if [ -z "$VMNAME" ]; then
-    echo "Usage: $0 <VM_NAME>"
-    exit 1
-fi
+VMFOLDER="$(pwd)/vm"
+VMIMAGESFOLDER="$VMFOLDER/images"
 
 # Error handling function
 handle_error() {
@@ -13,49 +9,87 @@ handle_error() {
     exit 1
 }
 
+# Check if VM name is provided
+if [ -z "$VMNAME" ]; then
+    echo "Usage: $0 <VM_NAME>"
+    exit 1
+fi
+
+# Install necessary packages if not already installed
+#if ! command -v virt-install &>/dev/null; then
+#    sudo apt update
+#    sudo apt install -y virtinst
+#    sudo apt install libvirt-daemon-system libvirt-clients bridge-utils virtinst virt-manager -y
+#fi
+
+# Create folder if not exists and assign user:group
+if [ ! -d "$VMFOLDER" ]; then
+    # Create the folder if it doesn't exist
+    mkdir -p "$VMFOLDER"
+    chown libvirt-qemu:kvm $VMFOLDER
+    echo "Folder created successfully at $VMFOLDER"
+else
+    echo "Folder already exists at $VMFOLDER"
+fi
+# Create folder if not exists and assign user:group
+if [ ! -d "$VMIMAGESFOLDER" ]; then
+    # Create the folder if it doesn't exist
+    mkdir -p "$VMIMAGESFOLDER"
+    chown libvirt-qemu:kvm $VMIMAGESFOLDER
+    echo "Folder created successfully at $VMIMAGESFOLDER"
+else
+    echo "Folder already exists at $VMIMAGESFOLDER"
+fi
+
 # Download Debian ISO if not available
-if [ ! -f ./Debian.iso ]; then
-    wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso -O Debian.iso || handle_error "Failed to download Debian ISO"
+if [ ! -f $VMIMAGESFOLDER/Debian.iso ]; then
+    wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso -O $VMIMAGESFOLDER/Debian.iso || handle_error "Failed to download Debian ISO"
 fi
 
 # Download Guest Additions ISO if not available
-if [ ! -f ./VBoxGuestAdditions.iso ]; then
-    wget https://download.virtualbox.org/virtualbox/6.1.30/VBoxGuestAdditions_6.1.30.iso -O VBoxGuestAdditions.iso || handle_error "Failed to download Guest Additions ISO"
+if [ ! -f $VMIMAGESFOLDER/VBoxGuestAdditions.iso ]; then
+    wget https://download.virtualbox.org/virtualbox/6.1.30/VBoxGuestAdditions_6.1.30.iso -O $VMIMAGESFOLDER/VBoxGuestAdditions.iso || handle_error "Failed to download Guest Additions ISO"
 fi
 
-# Create VM
-VBoxManage createvm --name "$VMNAME" --ostype "Debian_64" --register --basefolder "$(pwd)" || handle_error "Failed to create $VMNAME VM"
+# Shutdown and Remove VM if already exists # TODO
+# virsh suspend $VMNAME
+# virsh shutdown $VMNAME
+# virsh shutdown --domain VM_NAME
+# virsh destroy $VMNAME
+# virsh destroy --domain VM_NAME
+# virsh undefine $VMNAME
+# virsh undefine --domain $VMNAME
+# virsh connect $VMNAME
+# virsh uri
+# systemctl start virtqemud.socket
+# systemctl is-enabled virtqemud # Failed to get unit file state for virtqemud.service: No such file or directory
+# systemctl status libvirtd
+# virsh -c qemu:///system list
+# virsh connect qemu:///session
+# https://www.techotopia.com/index.php/Installing_a_KVM_Guest_OS_from_the_Command-line_(virt-install)
+# -w NETWORK, --network=NETWORK bridge:BRIDGE
+# --network=bridge:br0
+# virsh console PTIDebian
+# extra-args='console=tty0 console=ttyS0,115200n8 serial'
+# --vnc
 
-# Enable I/O APIC support
-VBoxManage modifyvm "$VMNAME" --ioapic on || handle_error "Failed to enable I/O APIC"
-
-# Set memory
-VBoxManage modifyvm "$VMNAME" --memory 2048 --vram 128 || handle_error "Failed to set memory"
-
-# Create Disk
-VBoxManage createhd --filename "$(pwd)/$VMNAME/$VMNAME_DISK.vdi" --size 50000 --format VDI || handle_error "Failed to create disk"
-VBoxManage storagectl "$VMNAME" --name "SATA Controller" --add sata --controller IntelAhci || handle_error "Failed to add SATA controller"
-VBoxManage storageattach "$VMNAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$(pwd)/$VMNAME/$VMNAME_DISK.vdi" || handle_error "Failed to attach disk"
-
-# Add IDE Controller
-VBoxManage storagectl "$VMNAME" --name "IDE Controller" --add ide --controller PIIX4 || handle_error "Failed to add IDE controller"
-
-# Mount Debian ISO
-VBoxManage storageattach "$VMNAME" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "$(pwd)/Debian.iso" --mtype readonly || handle_error "Failed to attach Debian ISO"
-
-# Mount Guest Additions ISO
-VBoxManage storageattach "$VMNAME" --storagectl "IDE Controller" --port 1 --device 1 --type dvddrive --medium "$(pwd)/VBoxGuestAdditions.iso" || handle_error "Failed to attach Guest Additions ISO"
-
-# Set boot priority
-VBoxManage modifyvm "$VMNAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none || handle_error "Failed to set boot priority"
-
-# Enable RDP (Remote Desktop Protocol)
-VBoxManage modifyvm "$VMNAME" --vrde on || handle_error "Failed to enable RDP"
-VBoxManage modifyvm "$VMNAME" --vrdemulticon on --vrdeport 3389 || handle_error "Failed to configure RDP"
-
-# Add network card and enable services
-VBoxManage modifyvm "$VMNAME" --nic1 nat || handle_error "Failed to add network card"
-VBoxManage modifyvm "$VMNAME" --natpf1 "ssh,tcp,,3022,,22" || handle_error "Failed to configure port forwarding"
+# Create VM using virt-install
+virt-install \
+    --name "$VMNAME" \
+    --memory 2048 \
+    --vcpus 1 \
+    --disk size=50 \
+    --cdrom "$VMIMAGESFOLDER/Debian.iso" \
+    --location "$VMIMAGESFOLDER/Debian.iso" \
+    --network default \
+    --graphics none \
+    --noautoconsole \
+    --os-variant debian12 \
+    --boot cdrom,hd \
+    --extra-args 'console=ttyS0,115200n8 serial' \
+    #--vnc \
+    #--os-type linux \
 
 # Start the VM once ready
-VBoxHeadless --startvm "$VMNAME" || handle_error "Failed to start $VMNAME VM"
+# virsh start "$VMNAME" || handle_error "Failed to start $VMNAME VM"
+virsh start "$VMNAME"
---
+++ create_vm_with_vbox.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+VMNAME="$1"
+
+# Check if VM name is provided
+if [ -z "$VMNAME" ]; then
+    echo "Usage: $0 <VM_NAME>"
+    exit 1
+fi
+
+# Error handling function
+handle_error() {
+    echo "Error: $1" >&2
+    exit 1
+}
+
+# Download Debian ISO if not available
+if [ ! -f ./Debian.iso ]; then
+    wget https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-12.5.0-amd64-netinst.iso -O Debian.iso || handle_error "Failed to download Debian ISO"
+fi
+
+# Download Guest Additions ISO if not available
+if [ ! -f ./VBoxGuestAdditions.iso ]; then
+    wget https://download.virtualbox.org/virtualbox/6.1.30/VBoxGuestAdditions_6.1.30.iso -O VBoxGuestAdditions.iso || handle_error "Failed to download Guest Additions ISO"
+fi
+
+# Create VM
+VBoxManage createvm --name "$VMNAME" --ostype "Debian_64" --register --basefolder "$(pwd)" || handle_error "Failed to create $VMNAME VM"
+
+# Enable I/O APIC support
+VBoxManage modifyvm "$VMNAME" --ioapic on || handle_error "Failed to enable I/O APIC"
+
+# Set memory
+VBoxManage modifyvm "$VMNAME" --memory 2048 --vram 128 || handle_error "Failed to set memory"
+
+# Create Disk
+VBoxManage createhd --filename "$(pwd)/$VMNAME/$VMNAME_DISK.vdi" --size 50000 --format VDI || handle_error "Failed to create disk"
+VBoxManage storagectl "$VMNAME" --name "SATA Controller" --add sata --controller IntelAhci || handle_error "Failed to add SATA controller"
+VBoxManage storageattach "$VMNAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$(pwd)/$VMNAME/$VMNAME_DISK.vdi" || handle_error "Failed to attach disk"
+
+# Add IDE Controller
+VBoxManage storagectl "$VMNAME" --name "IDE Controller" --add ide --controller PIIX4 || handle_error "Failed to add IDE controller"
+
+# Mount Debian ISO
+VBoxManage storageattach "$VMNAME" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "$(pwd)/Debian.iso" --mtype readonly || handle_error "Failed to attach Debian ISO"
+
+# Mount Guest Additions ISO
+VBoxManage storageattach "$VMNAME" --storagectl "IDE Controller" --port 1 --device 1 --type dvddrive --medium "$(pwd)/VBoxGuestAdditions.iso" || handle_error "Failed to attach Guest Additions ISO"
+
+# Set boot priority
+VBoxManage modifyvm "$VMNAME" --boot1 dvd --boot2 disk --boot3 none --boot4 none || handle_error "Failed to set boot priority"
+
+# Enable RDP (Remote Desktop Protocol)
+VBoxManage modifyvm "$VMNAME" --vrde on || handle_error "Failed to enable RDP"
+VBoxManage modifyvm "$VMNAME" --vrdemulticon on --vrdeport 3389 || handle_error "Failed to configure RDP"
+
+# Add network card and enable services
+VBoxManage modifyvm "$VMNAME" --nic1 nat || handle_error "Failed to add network card"
+VBoxManage modifyvm "$VMNAME" --natpf1 "ssh,tcp,,3022,,22" || handle_error "Failed to configure port forwarding"
+
+# Start the VM once ready
+VBoxHeadless --startvm "$VMNAME" || handle_error "Failed to start $VMNAME VM"
